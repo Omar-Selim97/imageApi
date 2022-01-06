63a1c764c0c8e698866e12578901c8dd3e45f48a
Omar Selim <87210693+Omar-Selim97@users.noreply.github.com>
2022-01-06 21:36:03 +0200

    Update resize.ts

--- src/middelware/resize.ts
+++ src/middelware/resize.ts
@@ -8,25 +8,27 @@ import { imgPath, thumbnailPath } from '../path/getpath'
 
 // resize our image with sharp
 const resiezeImg = async (fileName: string, width: string, height: string): Promise<void> => {
-  await sharp(path.join(imgPath, `${fileName}.jpg`))
-    .resize(parseInt(width, 10), parseInt(height, 10))
-    .toFile(path.join(thumbnailPath, `${fileName}${width}x${height}.jpg`))
-    .catch((err: Error) => {
-      throw err
-    })
+  if (+width > 0 && +width < 1000 && +height > 0 && +height < 1000) {
+    await sharp(path.join(imgPath, `${fileName}.jpg`))
+      .resize(parseInt(width, 10), parseInt(height, 10))
+      .toFile(path.join(thumbnailPath, `${fileName}${width}x${height}.jpg`))
+      .catch((err) => {
+        throw new Error(`'whoops' ${err}`)
+      })
+  }
 }
 
 // return img when put url
 // eslint-disable-next-line consistent-return
-const getImage = (fileName: string, width: string, height: string) => {
+const getImage = (fileName: string, width: string, height: string): unknown => {
   try {
     const catchImage = fs.readFileSync(
       path.join(thumbnailPath, `${fileName}${width}x${height}.jpg`)
     )
 
     return catchImage
-  } catch (error) {
-    console.log(error)
+  } catch (err) {
+    throw new Error(`'whoops width and height : must be more than 0 and less than 1000 ' ${err}`)
   }
 }
 
